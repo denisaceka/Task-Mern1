84c9b59e80c6ded606a49eca692182d800f2c90d
student <student@uet.com>
2026-05-12 18:33:22 +0200

    Update project

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,20 +1,31 @@
 const getTasks = (req, res) => {
-		res.status(200).json({ message: 'Get All Tasks' });
-	}
+res.status(200).json({
+message: 'Get All Tasks',
+});
+};
+
 const setTask = (req, res) => {
-	res.status(200).json({ message: 'Create Task' });
-}
+if(!req.body || !req.body.text){
+		res.status(400)
+		throw new Error('Please enter a task');
+			}
+};
 
 const updateTask = (req, res) => {
-	res.status(200).json({ message: `Task ${req.params.id} updated.` });
-}
+res.status(200).json({
+message: `Task ${req.params.id} updated.`,
+});
+};
 
 const deleteTask = (req, res) => {
-	res.status(200).json({ message: `Task ${req.params.id}  deleted.` });
-}
+res.status(200).json({
+message: `Task ${req.params.id} deleted.`,
+});
+};
 
-
-
-
-
-    module.exports = {getTasks, setTask, updateTask, deleteTask};
+module.exports = {
+getTasks,
+setTask,
+updateTask,
+deleteTask,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
- const dotenv = require('dotenv').config();
-const port = process.env.PORT || 8000 ;
+const dotenv = require('dotenv').config();
+
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const port = process.env.PORT || 8000;
+
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
- app.listen(port, () => console.log(`Server listening on ${port}`));
+// Error middleware
+app.use(errorHandler);
+
+// Start server
+app.listen(port, () => {
+console.log(`Server listening on port ${port}`);
+});
